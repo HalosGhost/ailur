0512568df38864f986f9faa56e38b09197150fe1
halosghost <halosghost@archlinux.info>
2016-06-27 16:50:02 -0500

    handle special character in a factoid

--- src/hgbot_factoids.lua
+++ src/hgbot_factoids.lua
@@ -1,8 +1,8 @@
 return
-  { ['ping']           = '🐼'
-  , ['hello']          = 'hai'
-  , ['are you a bot?'] = 'A bot? Me? Never!'
-  , ['ugm']            = 'Good (ugt) morning to all!'
-  , ['ugn']            = 'Good (ugt) night to all!'
-  , ['source']         = 'cf. <https://github.com/HalosGhost/irc_bot>'
+  { ['ping']            = '🐼'
+  , ['hello']           = 'hai'
+  , ['are you a bot%?'] = 'A bot? Me? Never!'
+  , ['ugm']             = 'Good (ugt) morning to all!'
+  , ['ugn']             = 'Good (ugt) night to all!'
+  , ['source']          = 'cf. <https://github.com/HalosGhost/irc_bot>'
   }
