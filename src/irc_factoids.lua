6ef4531db9ad9c6266ac55142b5e31ae23ede72f
halosghost <halosghost@archlinux.info>
2016-08-01 16:38:22 -0500

    More simple factoids

--- src/irc_factoids.lua
+++ src/irc_factoids.lua
@@ -7,6 +7,8 @@ local self =
   , ['help']           = 'see `listfacts` and `listaliases`'
   , ['﻿']         = 'Halp! I\'ze been haXXed!'
   , ['stahp']          = 'ಠ_ಠ'
+  , ['best']           = 'Use what is best for \x1Dyou!\x1D'
+  , ['halosghost']     = 'Do what feels right!'
   }
 
 return self
