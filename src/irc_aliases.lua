b7bce505a749e05c088a4e652bb08463410903cf
halosghost <halosghost@archlinux.info>
2016-09-26 21:35:10 -0500

    make it fancy-lookin for Lola

--- src/irc_aliases.lua
+++ src/irc_aliases.lua
@@ -237,9 +237,9 @@ local self =
           if resp then
               if ms.debug then print(resp) end
               local res = json.decode(resp)
-              local lnk = res[4][1] ~= '' and res[4][1] or 'Something went wrong'
-              local dsc = res[3][1] ~= '' and res[3][1] or 'No description found'
-              ms.irc.privmsg(c, t, lnk .. ' - ' .. dsc)
+              local lnk = res[4][1] ~= '' and res[4][1] or 'No results'
+              local dsc = res[3][1] ~= '' and ' - ' .. res[3][1] or ''
+              ms.irc.privmsg(c, t, lnk .. dsc)
               return
           end
       end
