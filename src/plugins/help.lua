01f40193dfbcd7c48864aad3abbd7874183845bf
halosghost <samuel.stuewe@gmail.com>
2019-01-24 22:42:33 -0600

    add help plugin

---
+++ src/plugins/help.lua
@@ -0,0 +1,19 @@
+local help = {}
+
+help.help = 'usage: help <plugin>'
+
+help.main = function(args)
+    local _, _, mod = args.message:find('(%S+)')
+
+    if args.modules.config.debug then
+        print(mod, args.modules.plugins[mod], args.modules.plugins[mod].help)
+    end
+
+    local usage = type(args.modules.plugins[mod]) == 'table'
+        and args.modules.plugins[mod].help
+        or 'No help available on that topic'
+
+    args.modules.irc.privmsg(args.connection, args.target, usage)
+end
+
+return help
