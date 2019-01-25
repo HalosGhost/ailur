e0f5c04aca4f65b1c3df14c19026169ff7058a43
halosghost <samuel.stuewe@gmail.com>
2019-01-24 22:40:51 -0600

    load plugins

--- src/main.lua
+++ src/main.lua
@@ -4,11 +4,25 @@ while true do
     local ms = {}
     ms.extload = require 'extload'
 
-    for f in lfs.dir(lfs.currentdir()) do
+    local cwd = lfs.currentdir()
+
+    for f in lfs.dir(cwd) do
         local _, _, mod = f:find('(.+)%.lua$')
         if mod and mod ~= 'main' and not ms[mod] then
             ms.extload(ms, mod)
         end
+    end
+
+    ms.plugins = {}
+    local stat = lfs.chdir('plugins')
+    if stat then
+        for f in lfs.dir(lfs.currentdir()) do
+            local _, _, plugin = f:find('(.+)%.lua')
+            if plugin and not ms.plugins[plugin] then
+                ms.extload(ms.plugins, plugin)
+            end
+        end
+        lfs.chdir(cwd)
     end
 
     local main = nil
