5036286cb60a01e582b2154178b37da6ececeab2
halosghost <samuel.stuewe@gmail.com>
2019-03-02 10:10:48 -0600

    preliminary duckduckgo searching (supports bangs!)

---
+++ src/plugins/duckduckgo.lua
@@ -0,0 +1,48 @@
+local json = require 'json'
+local url = require 'socket.url'
+local https = require 'ssl.https'
+
+local duckduckgo = {}
+
+duckduckgo.help = 'Usage: duckduckgo <search>'
+
+local plain = function(resp)
+    local _, _, link = resp:find('</table>.-</table>.-<a rel="nofollow" href="(.-)" class=\'result%-link\'>')
+    return link or 'No Results'
+end
+
+local bang = function(resp)
+    local data = json.decode(resp)
+    if data and data.Redirect and data.Redirect ~= '' then
+        return data.Redirect
+    else
+        return 'No Results'
+    end
+end
+
+duckduckgo.main = function(args)
+    local _, _, search = args.message:find('(.+)')
+
+    if args.modules.config.debug then
+        print(search)
+    end
+
+    if not search then
+        args.modules.irc.privmsg(args.connection, args.target, 'You want me to search for what?')
+    end
+
+    local target = search:find('^!')
+        and 'https://api.duckduckgo.com/?q=%s&format=json&no_html=1&no_redirect=1'
+        or 'https://duckduckgo.com/lite/?q=%s&kl=us-en&k1=-1&kd=-1&kp=1'
+
+    local resp = https.request(target:format(url.escape(search)))
+    if resp then
+        if args.modules.config.debug then print(resp) end
+        args.modules.irc.privmsg(args.connection, args.target, search:find('^!') and bang(resp) or plain(resp))
+        return
+    end
+
+    args.modules.irc.privmsg(args.connection, args.target, 'something went wrong with the request')
+end
+
+return duckduckgo
