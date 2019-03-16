e227ec29c45c1eb94c0aa208b56ec1d44ab37fcc
Thomas Berryhill <oats@oatberry.me>
2019-03-15 20:06:50 -0400

    rename moderate plugin to 'ircctl'

--- src/plugins/ircctl.lua
+++ src/plugins/ircctl.lua
@@ -1,24 +1,24 @@
 -- channel moderation
 
-local moderate = {}
+local ircctl = {}
 
-moderate.commands = {}
+ircctl.commands = {}
 
-moderate.commands.kick = function (args)
+ircctl.commands.kick = function (args)
     local _, _, recipient, message = args.message:find('kick%s+(%S+)%s*(.*)')
 
     if args.modules.config.debug then print(('kicking %s'):format(recipient)) end
     args.modules.irc.kick(args.connection, args.target, recipient, message or recipient)
 end
 
-moderate.commands['set-mode'] = function (args)
+ircctl.commands['set-mode'] = function (args)
     local _, _, mode, recipient = args.message:find('([+-][bqvo])%s+(.+)')
 
     if args.modules.config.debug then print(('setting %s to %s'):format(recipient, mode)) end
     args.modules.irc.modeset(args.connection, args.target, recipient, mode)
 end
 
-moderate.commands.join = function (args)
+ircctl.commands.join = function (args)
     local _, _, channel = args.message:find('join%s+(%S+)')
 
     if channel then
@@ -27,7 +27,7 @@ moderate.commands.join = function (args)
     end
 end
 
-moderate.commands.hatroulette = function (args)
+ircctl.commands.hatroulette = function (args)
     local md = { 'q', 'b', 'v', 'o', 'kick'}
     local mode_roll = md[math.random(#md)]
 
@@ -41,18 +41,18 @@ moderate.commands.hatroulette = function (args)
 end
 
 local h = ''
-for k in pairs(moderate.commands) do
+for k in pairs(ircctl.commands) do
     h = ('%s|%s'):format(h, k)
 end
-moderate.help = ('usage: moderate <%s>'):format(h:sub(2))
+ircctl.help = ('usage: ircctl <%s>'):format(h:sub(2))
 
-moderate.main = function (args)
+ircctl.main = function (args)
     local _, _, action = args.message:find('(%S+)')
-    local f = moderate.commands[action]
+    local f = ircctl.commands[action]
 
     if args.authorized and f then return f(args) end
 
-    args.modules.irc.privmsg(args.connection, args.target, moderate.help)
+    args.modules.irc.privmsg(args.connection, args.target, ircctl.help)
 end
 
-return moderate
+return ircctl
