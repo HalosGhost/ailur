b6badd9b0e8ae15ae4844b2e7ed5fd4340c50dcf
halosghost <samuel.stuewe@gmail.com>
2019-04-03 17:19:40 -0500

    remove errant require and aliases which are easy to make macros for

--- src/irc_aliases.lua
+++ src/irc_aliases.lua
@@ -1,16 +1,4 @@
-local mediawiki_alias = require 'mediawiki_alias'
-
 local aliases = {}
-
-aliases['ug[maen]'] = function (ms, t, msg, _, s)
-    local map = { ['m'] = 'Morning'
-                , ['a'] = 'Afternoon'
-                , ['e'] = 'Evening'
-                , ['n'] = 'Night'
-                }
-    local _, _, l = msg:find('ug(.)')
-    ms.irc.privmsg(t, ('%s says “Good (ᴜɢᴛ) %s to all!”'):format(s, map[l]))
-end
 
 aliases['say%s+.+'] = function (ms, t, msg)
     local _, _, m = msg:find('say%s+(.+)')
@@ -31,21 +19,6 @@ aliases['give%s+%S+.+'] = function (ms, t, msg, _, sndr)
         end
         ms.irc.privmsg(t, to .. ': ' .. (thing or (sndr .. ' wanted you to have ' .. what)))
     end
-end
-
-aliases['you.*'] = function (ms, t, msg, _, sndr)
-    local _, _, attr = msg:find('you(.*)')
-    ms.irc.privmsg(t, ('%s: No, \x1Dyou\x1D%s!'):format(sndr, attr or ''))
-end
-
-aliases['bloat%s*.*'] = function (ms, t, msg, _, sndr)
-    local _, _, target = msg:find('bloat%s*(.*)')
-    target = target == '' and sndr or target
-    ms.irc.privmsg(t, target .. ' is bloat.')
-end
-
-aliases['judges'] = function (ms, t, _, _, sndr)
-    ms.irc.privmsg(t, ('So close, but %s won by a nose!'):format(sndr))
 end
 
 return aliases
